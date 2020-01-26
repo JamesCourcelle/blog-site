9d39cb18c4dd222975d4cba0ebbdd4da13a89bef
James Courcelle <james.courcelle@snhu.edu>
2020-01-26 10:34:57 -0500

    Ensured the springboot application can utilize the MongoRepository functionality

---
+++ src/main/java/blog/site/BlogApplication.java
@@ -0,0 +1,49 @@
+package blog.site;
+
+import blog.site.entities.Customer;
+import blog.site.repositories.TestRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogApplication implements CommandLineRunner {
+    @Autowired
+    private TestRepository repository;
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        repository.deleteAll();
+
+        // save a couple of customers
+        repository.save(new Customer("Alice", "Smith"));
+        repository.save(new Customer("Bob", "Smith"));
+
+        // fetch all customers
+        System.out.println("Customers found with findAll():");
+        System.out.println("-------------------------------");
+        for (Customer customer : repository.findAll()) {
+            System.out.println(customer);
+        }
+        System.out.println();
+
+        // fetch an individual customer
+        System.out.println("Customer found with findByFirstName('Alice'):");
+        System.out.println("--------------------------------");
+        System.out.println(repository.findByFirstName("Alice"));
+
+        System.out.println("Customers found with findByLastName('Smith'):");
+        System.out.println("--------------------------------");
+        for (Customer customer : repository.findByLastName("Smith")) {
+            System.out.println(customer);
+        }
+
+    }
+
+}
---
+++ src/main/java/blog/site/entities/Customer.java
@@ -0,0 +1,28 @@
+package blog.site.entities;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(value = "inventory")
+public class Customer {
+
+    @Id
+    private String id;
+
+    private String firstName;
+    private String lastName;
+
+    public Customer() {}
+
+    public Customer(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Customer[id=%s, firstName='%s', lastName='%s']",
+                id, firstName, lastName);
+    }
+}
